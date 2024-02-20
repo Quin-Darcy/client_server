b662174cde9919345a6005ca1e045928eae29abe
Quin-Darcy <pohmsuindraguli@gmail.com>
2024-02-20 09:28:25 -0800

    Added more to the load_sections functions and included a cleanup function

--- simple_client/execution.c
+++ simple_client/execution.c
@@ -5,19 +5,54 @@
 #include <winnt.h>
 #include "execution.h"
 
+typedef struct _SECTION_INFO {
+    LPVOID base_address; // The address at which the section was loaded in memory
+    DWORD section_size;  // The size of the section in memory
+} SECTION_INFO;
+
 typedef struct _PE_CONTEXT {
-    LPVOID image_base; // Preferred base address
+    DWORD image_base; // Preferred base address
     DWORD address_of_entry; // Address at which the entry point is located
     DWORD number_of_sections; // Number of sections
     IMAGE_SECTION_HEADER* section_headers; // Pointer to the first section header
+    SECTION_INFO* sections_info; // Stores the actual base address of each section and its size
 } PE_CONTEXT; 
 
+
+void cleanup(PE_CONTEXT* pe_ctx)
+{
+    printf("[+] Cleaning up ...\n");
+
+    // Free the memory which was allocated to the sections_headers and sections_info members
+    if (pe_ctx->section_headers != NULL)
+    {
+        free(pe_ctx->section_headers);
+    }
+
+    // Loop through and free all the allocated memory for each section
+    for (DWORD i = 0; i < pe_ctx->number_of_sections; i++)
+    {
+        SECTION_INFO* current_section_info = &pe_ctx->sections_info[i];
+
+        if (!VirtualFree(current_section_info->base_address, 0, MEM_RELEASE))
+        {
+            fprintf(stderr, "[!] Failed to free one of the sections.\n");
+        }
+    }
+
+    // Free the memory allocated to sections_info
+    if (pe_ctx->sections_info != NULL)
+    {
+        free(pe_ctx->sections_info);
+    }
+}
 
 // Parse the PE file and load each section individually
 int load_sections(const unsigned char* payload, PE_CONTEXT* pe_ctx)
 {
-    printf("[+] Loading sections ...\n");
+    printf("[+] Loading PE sections ...\n");
 
+    // Check for valid DOS signature
     const IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)payload;
     if (dos_header->e_magic != 0x5A4D)
     {
@@ -25,6 +60,7 @@ int load_sections(const unsigned char* payload, PE_CONTEXT* pe_ctx)
         return 1;
     }
 
+    // Check that its a valid PE 
     IMAGE_NT_HEADERS32* nt_headers = (IMAGE_NT_HEADERS32*)(payload + dos_header->e_lfanew);
     if (nt_headers->Signature != IMAGE_NT_SIGNATURE)
     {
@@ -32,6 +68,7 @@ int load_sections(const unsigned char* payload, PE_CONTEXT* pe_ctx)
         return 1;
     }
 
+    // Check its architecture
     if (nt_headers->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) 
     {
         fprintf(stderr, "[!] 64-bit architechture detected. Please use 32-bit instead.\n");
@@ -47,13 +84,67 @@ int load_sections(const unsigned char* payload, PE_CONTEXT* pe_ctx)
     pe_ctx->image_base = nt_headers->OptionalHeader.ImageBase;
     pe_ctx->address_of_entry = nt_headers->OptionalHeader.AddressOfEntryPoint;
     pe_ctx->number_of_sections = nt_headers->FileHeader.NumberOfSections; 
-    pe_ctx->section_headers = (IMAGE_SECTION_HEADER*)(nt_headers + 1);
 
-    // Here we will populate the section_headers array with each section;s header
+    printf("[i] Number of Sections: %lu\n", pe_ctx->number_of_sections);
+    
+    // Allocate memory to store the all the section headers
+    size_t section_headers_size = sizeof(IMAGE_SECTION_HEADER) * pe_ctx->number_of_sections;
+    pe_ctx->section_headers = (IMAGE_SECTION_HEADER*)calloc(section_headers_size, 1);
 
-    // Here we will map each section into memory and update the corresponding section header
-    // to contain the mapped values, mainly PointerToRawData which should be the address 
-    // at which the section was mapped?
+    if (pe_ctx->section_headers == NULL)
+    {
+        fprintf(stderr, "[!] Failed to allocate memory for section headers.\n");
+        return 1;
+    }
+
+    // Copy section_headers_size many bytes starting from the beginning of the first section header at nt_headers + 1
+    memcpy(pe_ctx->section_headers, (void*)(nt_headers + 1), section_headers_size);
+
+    // Allocate memory for the sections_info member
+    size_t sections_size = sizeof(SECTION_INFO) * pe_ctx->number_of_sections;
+    pe_ctx->sections_info = (SECTION_INFO*)calloc(sections_size, 1);
+
+    if (pe_ctx->sections_info == NULL)
+    {
+        fprintf(stderr, "[!] Failed to allocate memory for sections info.\n");
+        return 1;
+    }
+
+    // Loop through each section and map it into memory
+    for (DWORD i = 0; i < pe_ctx->number_of_sections; i++)
+    {
+        // Capture the current header and section info
+        IMAGE_SECTION_HEADER* current_section = &pe_ctx->section_headers[i];
+        SECTION_INFO* current_sections_info = &pe_ctx->sections_info[i];
+
+        // Attempt to allocate memory for it at its preferred address
+        LPVOID preferred_address = (LPVOID)(pe_ctx->image_base + current_section->VirtualAddress);
+        LPVOID base_addr = VirtualAlloc(preferred_address, (size_t)current_section->Misc.VirtualSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+        // If the preferred address is unavilable, let the OS decide
+        if (base_addr == NULL)
+        {
+            base_addr = VirtualAlloc(NULL, (size_t)current_section->Misc.VirtualSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+            // If it fails again, return
+            if (base_addr == NULL)
+            {
+                fprintf(stderr, "[!] Failed to allocate memory for the following section: %s\n", current_section->Name);
+                cleanup(pe_ctx);
+                return 1;
+            }
+        }
+
+        // Update the corresponding sections_info member
+        current_sections_info->base_address = base_addr;
+        current_sections_info->section_size = current_section->Misc.VirtualSize;
+
+        // Copy the contents of the section into the allocated region provided they are non-zero
+        if (current_section->SizeOfRawData > 0)
+        {
+            memcpy(base_addr, (void*)(payload + current_section->PointerToRawData), (size_t)current_section->SizeOfRawData);
+        }
+    }
 
     return 0;
 }
@@ -256,41 +347,48 @@ int resolve_imports(const unsigned char* base_address)
 
 int execute_payload(const unsigned char* payload, const size_t payload_size)
 {
-    DWORD image_base;
-    DWORD address_of_entry;
-    if (parse_headers(payload, &image_base, &address_of_entry) != 0)
-    {
-        fprintf(stderr, "[!] parse_headers() failed.\n");
-        return 1;
-    }
+    // DWORD image_base;
+    // DWORD address_of_entry;
+    // if (parse_headers(payload, &image_base, &address_of_entry) != 0)
+    // {
+    //     fprintf(stderr, "[!] parse_headers() failed.\n");
+    //     return 1;
+    // }
 
-    LPVOID base_address = NULL;
-    int result = allocate_executable_memory(payload_size, image_base, &base_address);
-    if (result == -1)
-    {
-        fprintf(stderr, "[!] allocate_executable_memory() failed.\n");
-        return 1;
-    }
+    // LPVOID base_address = NULL;
+    // int result = allocate_executable_memory(payload_size, image_base, &base_address);
+    // if (result == -1)
+    // {
+    //     fprintf(stderr, "[!] allocate_executable_memory() failed.\n");
+    //     return 1;
+    // }
 
-    printf("[+] Copying payload bytes into allocated memory region at 0x%p ...\n", base_address);
-    memcpy(base_address, payload, payload_size);
+    // printf("[+] Copying payload bytes into allocated memory region at 0x%p ...\n", base_address);
+    // memcpy(base_address, payload, payload_size);
 
-    // If previous result was 1, we need to apply relocations to the loaded EXE
-    if (result == 1)
-    {
-        apply_relocations(image_base, base_address);
-    }
+    // // If previous result was 1, we need to apply relocations to the loaded EXE
+    // if (result == 1)
+    // {
+    //     apply_relocations(image_base, base_address);
+    // }
 
-    if (resolve_imports((unsigned char*)base_address) != 0)
-    {
-        fprintf(stderr, "[!] resolve_imports() failed.\n");
-        return 1;
-    }
+    // if (resolve_imports((unsigned char*)base_address) != 0)
+    // {
+    //     fprintf(stderr, "[!] resolve_imports() failed.\n");
+    //     return 1;
+    // }
 
-    printf("[+] Freeing allocated memory ...\n");
-    if (!VirtualFree(base_address, (DWORD)payload_size, MEM_DECOMMIT))
+    // printf("[+] Freeing allocated memory ...\n");
+    // if (!VirtualFree(base_address, (DWORD)payload_size, MEM_DECOMMIT))
+    // {
+    //     fprintf(stderr, "[!] VirtualFree() failed.\n");
+    //     return 1;
+    // }
+
+    PE_CONTEXT pe_ctx;
+    if (load_sections(payload, &pe_ctx) != 0)
     {
-        fprintf(stderr, "[!] VirtualFree() failed.\n");
+        fprintf(stderr, "[!] Load sections failed.\n");
         return 1;
     }
 
